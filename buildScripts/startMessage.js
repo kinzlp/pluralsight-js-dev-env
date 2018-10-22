6a246fac4ac0f1172f782654f2c566727ff15be5
kinz <kinz@kinzs-iMac-2.local>
2018-10-22 23:55:33 +0800

    ES6

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,5 @@
 // var chalk = require('chalk')  // This is ES5 syntax
 
-import chalk from 'chalk';
+import chalk from 'chalk'; // This is ES6 syntax
 
 console.log(chalk.green('Start app in dev mode'))
